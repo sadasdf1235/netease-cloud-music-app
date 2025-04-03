fd139a1cc25ab9e4435f5f379cd390126e41a9fa
sadasdf1235 <tgb80576@163.com>
2025-04-03 09:31:39 +0800

    refactor: 优化API模块结构，移除冗余模块和导出
    1. 移除已弃用的musicApi和songApi模块\n2. 保留并优化musicSongApi作为统一模块\n3. 扩展直接导出的常用函数\n4. 清理过时的注释和警告信息

--- src/api/index.ts
+++ src/api/index.ts
@@ -10,35 +10,32 @@ export { default as request, get, post } from '@/utils/request';
 import * as albumApi from './modules/album';
 import * as artistApi from './modules/artist';
 import * as commentApi from './modules/comment';
-import * as musicApi from './modules/music';
+import * as musicSongApi from './modules/music-song'; // 合并后的主要模块
 import * as mvApi from './modules/mv';
-import * as musicSongApi from './modules/music-song'; // 新的合并模块
 import * as playlistApi from './modules/playlist';
 import * as searchApi from './modules/search';
-import * as songApi from './modules/song';
 import * as toplistApi from './modules/toplist';
 import * as userApi from './modules/user';
 
-// 注意: music.ts和song.ts中存在重复的API函数
-// 这些重复函数已合并到music-song.ts中
-// TODO: 后续应该完全迁移到musicSongApi，移除musicApi和songApi
-
+// 导出所有API模块
 export {
   albumApi,
   artistApi,
   commentApi,
-  musicApi,
   mvApi,
-  musicSongApi, // 导出新的合并模块
+  musicSongApi, // 音乐和歌曲API的统一模块
   playlistApi,
   searchApi,
-  songApi,
   toplistApi,
   userApi
 };
 
-// 为了兼容性，直接导出一些常用函数
-// 现在从合并后的模块导出
+// 直接导出常用函数，方便使用
 export const getBanners = musicSongApi.getBanners;
 export const getToplist = toplistApi.getAllToplist;
 export const getToplistDetail = toplistApi.getToplistDetail;
+export const getSongUrl = musicSongApi.getSongUrl;
+export const getSongDetail = musicSongApi.getSongDetail;
+export const getLyric = musicSongApi.getLyric;
+export const getRecommendPlaylists = musicSongApi.getRecommendPlaylists;
+export const getNewSongs = musicSongApi.getNewSongs;
